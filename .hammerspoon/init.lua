e34daea30534762924eb944ad039824724db7efb
Parker Smith <parker@parkersmith.us>
2026-03-22 15:24:44 -0600

    Migrate from wezterm to ghostty

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -12,7 +12,7 @@ end
 hs.hotkey.bind({"cmd", "alt", "ctrl", "shift"}, "E", open_app("Visual Studio Code"))
 -- hs.hotkey.bind({"cmd", "alt", "ctrl", "shift"}, "E", open_app("Visual Studio Code - Insiders"))
 -- hs.hotkey.bind({"cmd", "alt", "ctrl", "shift"}, "F", open_app("Firefox"))
-hs.hotkey.bind({"cmd", "alt", "ctrl", "shift"}, "T", open_app("WezTerm"))
+hs.hotkey.bind({"cmd", "alt", "ctrl", "shift"}, "T", open_app("Ghostty"))
 hs.hotkey.bind({"cmd", "alt", "ctrl", "shift"}, "C", open_app("Google Chrome"))
 hs.hotkey.bind({"cmd", "alt", "ctrl", "shift"}, "B", open_app("Sublime Text"))
 hs.hotkey.bind({"cmd", "alt", "ctrl", "shift"}, "W", open_app("Finder"))
